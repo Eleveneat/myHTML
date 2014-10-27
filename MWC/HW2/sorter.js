cc2430e3ceab01d258809209cae2fb1d4975217b
Eleven <eleveneat@gmail.com>
2014-10-27 14:35:16 +0800

    HW2_version 1.

--- MWC/HW2/sorter.js
+++ MWC/HW2/sorter.js
@@ -22,11 +22,40 @@ function reset_Other_Th(ths) {
 		ths[i].addEventListener('click',toSort_ascending);
 	}
 }
+
+function tr_Exchang(tds_1, tds_2) {
+	var len = tds_1.length;
+	for (var i = 0; i < len; i++) {
+		var tmp = tds_1[i].firstChild.nodeValue;
+		tds_1[i].firstChild.nodeValue = tds_2[i].firstChild.nodeValue;
+		tds_2[i].firstChild.nodeValue = tmp;
+	}
+}
+
 function toSort_ascending(event) {
-	var table = this.parentNode.parentNode.parentNode;
-	//alert(table.nodeName);
-	var ths = table.getElementsByTagName('th');
+	var sortedTable = this.parentNode.parentNode.parentNode;
+	var ths = sortedTable.getElementsByTagName('th');
 	reset_Other_Th(ths);
+
+
+	var th_pos = -1;
+	for (var i = 0; i < ths.length; i++) {
+		if (ths[i].firstChild.nodeValue == this.firstChild.nodeValue)
+			th_pos = i;
+	}
+
+
+	var trs = sortedTable.getElementsByTagName('tr');
+	for (var j = 1; j < trs.length-1; j++) {
+		for (var i = 1; i < trs.length-j; i++) {
+			var tdsOfTr_1 = trs[i].getElementsByTagName('td');
+			var tdsOfTr_2 = trs[i+1].getElementsByTagName('td');
+			if (tdsOfTr_1[th_pos].firstChild.nodeValue > tdsOfTr_2[th_pos].firstChild.nodeValue) {
+				tr_Exchang(tdsOfTr_1, tdsOfTr_2);
+			}
+		}
+	}
+
 	this.removeEventListener('click', toSort_ascending);
 	this.style.backgroundColor = "rgba(150, 156, 255, 1)";
 	this.style.backgroundImage = "url(ascend.png)";
@@ -35,6 +64,28 @@ function toSort_ascending(event) {
 	this.addEventListener('click', toSort_descending);
 }
 function toSort_descending(event) {
+	var sortedTable = this.parentNode.parentNode.parentNode;
+	var ths = sortedTable.getElementsByTagName('th');
+
+
+	var th_pos = -1;
+	for (var i = 0; i < ths.length; i++) {
+		if (ths[i].firstChild.nodeValue == this.firstChild.nodeValue)
+			th_pos = i;
+	}
+
+
+	var trs = sortedTable.getElementsByTagName('tr');
+	for (var j = 1; j < trs.length-1; j++) {
+		for (var i = 1; i < trs.length-j; i++) {
+			var tdsOfTr_1 = trs[i].getElementsByTagName('td');
+			var tdsOfTr_2 = trs[i+1].getElementsByTagName('td');
+			if (tdsOfTr_1[th_pos].firstChild.nodeValue < tdsOfTr_2[th_pos].firstChild.nodeValue) {
+				tr_Exchang(tdsOfTr_1, tdsOfTr_2);
+			}
+		}
+	}
+
 	this.removeEventListener('click', toSort_descending);
 	this.style.backgroundImage = "url(descend.png)";
 	this.style.backgroundPosition = "top right";
